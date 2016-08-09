a94e134ab092949b8306d99ebf9c7625d5491d08
Tuomas Reunanen <tuomas.reunanen@aalto.fi>
2016-08-09 11:30:24 +0300

    Improved adventure starting ui, added icons and stat labels

--- app/src/main/scala/com/tume/scalarpg/ui/GameUI.scala
+++ app/src/main/scala/com/tume/scalarpg/ui/GameUI.scala
@@ -27,7 +27,7 @@ class GameUI extends UIView {
     val xpBar = UIBuilder.progressBar.main(0xff993399, 0xffcc12cc).absWidth(1f).height(0.035f).left().below(healthBar).id("xpBar")
     view += xpBar.pad()
     // Ability and potion buttons
-    val potionButton = UIBuilder(new UIPanelToggleButton()).size(0.175f).drawable(R.drawable.potions).left(0.05f).below(xpBar).id("potions")
+    val potionButton = UIBuilder.panelButton.img(R.drawable.potions).size(0.175f).left(0.05f).below(xpBar).id("potions")
     val ability0Button = UIBuilder.button.size(0.175f).rightOf(potionButton).alignTop(potionButton).id("ability0")
     val ability1Button = UIBuilder.button.size(0.175f).below(potionButton).alignLeft(potionButton).id("ability1")
     val ability2Button = UIBuilder.button.size(0.175f).rightOf(ability1Button).alignTop(ability1Button).id("ability2")
@@ -41,19 +41,19 @@ class GameUI extends UIView {
     view += ability4Button.pad(DisplayUtils.scale.toInt)
 
     // Movement buttons
-    val middlePoint = UIBuilder(new UISpace()).xBetween(ability0Button, right).yBetween(xpBar, bottom)
+    val middlePoint = UIBuilder.space.xBetween(ability0Button, right).yBetween(xpBar, bottom)
     val moveBtnSize = 0.15f
     val moveBtnSpace = moveBtnSize / 2
 
-    view += UIBuilder(new UIInstantButton).size(moveBtnSize).drawable(R.mipmap.ic_arrow_right).alignCenter(middlePoint).rightOf(middlePoint, moveBtnSpace).id("moveRight")
-    view += UIBuilder(new UIInstantButton).size(moveBtnSize).drawable(R.mipmap.ic_arrow_left).alignCenter(middlePoint).leftOf(middlePoint, moveBtnSpace).id("moveLeft")
-    view += UIBuilder(new UIInstantButton).size(moveBtnSize).drawable(R.mipmap.ic_arrow_up).alignCenter(middlePoint).above(middlePoint, moveBtnSpace).id("moveUp")
-    view += UIBuilder(new UIInstantButton).size(moveBtnSize).drawable(R.mipmap.ic_arrow_down).alignCenter(middlePoint).below(middlePoint, moveBtnSpace).id("moveDown")
+    view += UIBuilder.instantButton.img(R.mipmap.ic_arrow_right).size(moveBtnSize).alignCenter(middlePoint).rightOf(middlePoint, moveBtnSpace).id("moveRight")
+    view += UIBuilder.instantButton.img(R.mipmap.ic_arrow_left).size(moveBtnSize).alignCenter(middlePoint).leftOf(middlePoint, moveBtnSpace).id("moveLeft")
+    view += UIBuilder.instantButton.img(R.mipmap.ic_arrow_up).size(moveBtnSize).alignCenter(middlePoint).above(middlePoint, moveBtnSpace).id("moveUp")
+    view += UIBuilder.instantButton.img(R.mipmap.ic_arrow_down).size(moveBtnSize).alignCenter(middlePoint).below(middlePoint, moveBtnSpace).id("moveDown")
 
     val potionPanel = new UIPopupPanel()
-    val hpPotion = UIBuilder(new UIButton).size(0.15f).drawable(R.drawable.potion_ruby).left(0.05f).alignBottom(healthBar).id("healthPotion")
-    val manaPotion = UIBuilder(new UIButton).size(0.15f).drawable(R.drawable.potion_brilliant_blue).rightOf(hpPotion, 0.01f).alignBottom(healthBar).id("manaPotion")
-    val xpPotion = UIBuilder(new UIButton).size(0.15f).drawable(R.drawable.potion_emerald).rightOf(manaPotion, 0.01f).alignBottom(healthBar).id("xpPotion")
+    val hpPotion = UIBuilder.button.size(0.15f).img(R.drawable.potion_ruby).left(0.05f).alignBottom(healthBar).id("healthPotion")
+    val manaPotion = UIBuilder.button.size(0.15f).img(R.drawable.potion_brilliant_blue).rightOf(hpPotion, 0.01f).alignBottom(healthBar).id("manaPotion")
+    val xpPotion = UIBuilder.button.size(0.15f).img(R.drawable.potion_emerald).rightOf(manaPotion, 0.01f).alignBottom(healthBar).id("xpPotion")
     potionPanel += hpPotion.resolve
     potionPanel += manaPotion.resolve
     potionPanel += xpPotion.resolve
--- app/src/main/scala/com/tume/scalarpg/ui/HeroUI.scala
+++ app/src/main/scala/com/tume/scalarpg/ui/HeroUI.scala
@@ -2,6 +2,7 @@ package com.tume.scalarpg.ui
 
 import com.tume.engine.gui.UIView
 import com.tume.engine.gui.builder.UIBuilder
+import com.tume.scalarpg.R
 
 import scala.collection.mutable
 
@@ -13,27 +14,34 @@ class HeroUI extends UIView {
 
   override def build: Seq[UIBuilder] = {
     var view = mutable.Buffer[UIBuilder]()
-    var tmp: UIBuilder = UIBuilder.button
-    view += { tmp = UIBuilder.button.size(0.2f).top().left().id("hero0"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).top().rightOf(tmp).id("hero1"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).top().rightOf(tmp).id("hero2"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).top().rightOf(tmp).id("hero3"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).top().rightOf(tmp).id("hero4"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).below(tmp).left().id("hero5"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero6"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero7"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero8"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero9"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).below(tmp).left().id("hero10"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero11"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero12"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero13"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero14"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).below(tmp).left().id("hero15"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero16"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero17"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero18"); tmp }
-    view += { tmp = UIBuilder.button.size(0.2f).alignTop(tmp).rightOf(tmp).id("hero19"); tmp }
+    val ls = 0.06f // Stat & resistance label height
+    val rw = 0.15f // Resistance label width
+    val sw = 0.4f // Stat label width
+
+    var lab = UIBuilder.label
+    var first = UIBuilder.label
+    var panel = UIBuilder.panel
+    view += UIBuilder.button.size(0.2f).top().left().id("hero_select")
+    view += { panel=UIBuilder.panel.width(0.8f).height(0.42f).top().right(); panel }
+    // Name
+    view += { lab=UIBuilder.label.text("Knight").height(0.08f).width(0.8f).alignLeft(panel, 0.02f).alignTop(panel, 0.005f); lab }
+    // Stats
+    view += { lab=UIBuilder.label.img(R.drawable.ic_heart).text("122").height(ls).width(sw).alignLeft(panel, 0.02f).below(lab, 0.01f); first=lab; lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_mana).text("40").height(ls).width(sw).alignLeft(panel, 0.02f).below(lab); lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_sword).text("2-5").height(ls).width(sw).alignLeft(panel, 0.02f).below(lab); lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_staff).text("9").height(ls).width(sw).alignLeft(panel, 0.02f).below(lab); lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_shoe).text("5").height(ls).width(sw).alignLeft(panel, 0.02f).below(lab); lab }
+    // Res row 1
+    view += { lab=UIBuilder.label.img(R.drawable.ic_shield).text("5%").height(ls).width(rw).rightOf(first, 0.02f).alignTop(first); first=lab; lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_flame).text("2%").height(ls).width(rw).alignLeft(lab).below(lab); lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_frost).text("4%").height(ls).width(rw).alignLeft(lab).below(lab); lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_lightning).text("8%").height(ls).width(rw).alignLeft(lab).below(lab); lab }
+    // Res row 2
+    view += { lab=UIBuilder.label.img(R.drawable.ic_holy).text("7%").height(ls).width(rw).rightOf(first, 0.02f).alignTop(first); first=lab; lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_arcane).text("7%").height(ls).width(rw).alignLeft(lab).below(lab); lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_poison).text("1%").height(ls).width(rw).alignLeft(lab).below(lab); lab }
+    view += { lab=UIBuilder.label.img(R.drawable.ic_dark).text("3%").height(ls).width(rw).alignLeft(lab).below(lab); lab }
+
     view.foreach(_.pad())
     view
   }
